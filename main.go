74fa79c547d9a40d54ee552c86eadf336fb3e9d3
SaidMaratov <saidmaratov@gmail.com>
2021-04-15 22:16:43 +0600

    init: main file

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	//"os"
+	// "os/user"
+	// "strconv"
+	// "strings"
+	// "syscall"
+	// "time"
+	// "math/rand"
+	// "errors"
+)
+
+func main() {
+	fmt.Println("hey")
+}
